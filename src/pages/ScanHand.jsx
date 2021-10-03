ff5c7c8027c6c0b525db345ae8f7043ce49636c0
Sachin Desai <desai.sachin45@gmail.com>
2021-10-03 11:39:39 +0530

    wip

--- src/pages/ScanHand.jsx
+++ src/pages/ScanHand.jsx
@@ -14,10 +14,6 @@ const ScanHand = () => {
   const startCam = useCallback(() => {
     const constraints = {
       video: {
-        width: { min: 360, ideal: 360 },
-        height: { min: 640, ideal: 640 },
-        aspectRatio: 1.777777778,
-        frameRate: { max: 30 },
         facingMode: "environment",
       },
       audio: false,
@@ -46,15 +42,17 @@ const ScanHand = () => {
     <div className="appUi scanPage">
       <div className="scanContainer">
         <canvas id="canvas" width="360" height="640" ref={canRef}></canvas>
-        <video
-          width="360"
-          height="640"
-          id="video"
-          ref={vdRef}
-          autoPlay
-          playsInline
-          muted
-        ></video>
+        <div id="videoContainer">
+          <video
+            width="360"
+            height="640"
+            id="video"
+            ref={vdRef}
+            autoPlay
+            playsInline
+            muted
+          ></video>
+        </div>
       </div>
     </div>
   );
