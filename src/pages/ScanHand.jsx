53349845d27d653cbeea853de9ca0c7beb6916ea
Sachin Desai <desai.sachin45@gmail.com>
2021-10-03 12:32:17 +0530

    wip

--- src/pages/ScanHand.jsx
+++ src/pages/ScanHand.jsx
@@ -1,15 +1,51 @@
 import { useCallback, useEffect, useRef } from "react";
-
+import { Hands } from "@mediapipe/hands";
+import * as hands from "@mediapipe/hands";
+import * as cam from "@mediapipe/camera_utils";
+import { drawConnectors, drawLandmarks } from "@mediapipe/drawing_utils";
+import { useHistory } from "react-router";
 const ScanHand = () => {
+  const isComponentMounted = useRef({});
   const vdRef = useRef(null);
   const canRef = useRef(null);
   let currentStream = useRef();
+  let history = useHistory();
 
-  function stopMediaTracks(stream) {
-    stream.getTracks().forEach((track) => {
-      track.stop();
-    });
-  }
+  const onResults = useCallback((results) => {
+    let videoWidth = null;
+    let videoHeight = null;
+    if (vdRef != null) {
+      videoWidth = vdRef.current.videoWidth;
+      videoHeight = vdRef.current.videoHeight;
+    }
+
+    canRef.current.width = videoWidth;
+    canRef.current.height = videoHeight;
+
+    const canvasElement = canRef.current;
+    const canvasCtx = canvasElement.getContext("2d");
+
+    canvasCtx.save();
+    canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+    canvasCtx.drawImage(
+      results.image,
+      0,
+      0,
+      canvasElement.width,
+      canvasElement.height
+    );
+
+    if (results.multiHandLandmarks) {
+      for (const landmarks of results.multiHandLandmarks) {
+        drawConnectors(canvasCtx, landmarks, hands.HAND_CONNECTIONS, {
+          color: "#00FF00",
+          lineWidth: 2,
+        });
+        drawLandmarks(canvasCtx, landmarks, { color: "#00ffd0", lineWidth: 1 }); //#5d0db8 purple
+      }
+    }
+    canvasCtx.restore();
+  }, []);
 
   const startCam = useCallback(() => {
     const constraints = {
@@ -25,18 +61,50 @@ const ScanHand = () => {
         vdRef.current.srcObject = stream;
         return navigator.mediaDevices.enumerateDevices();
       })
-      .then()
+      .then(() => {
+        const hands = new Hands({
+          locateFile: (file) => {
+            return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
+          },
+        });
+        hands.setOptions({
+          maxNumHands: 2,
+          minDetectionConfidence: 0.5,
+          minTrackingConfidence: 0.5,
+        });
+        hands.onResults(onResults);
+
+        const camera = new cam.Camera(vdRef.current, {
+          onFrame: async () => {
+            await hands.send({ image: vdRef.current });
+          },
+          width: 360,
+          height: 640,
+        });
+        camera.start();
+      })
       .catch((error) => {
         console.error(error);
       });
   }, []);
 
+  function stopMediaTracks(stream) {
+    stream.getTracks().forEach((track) => {
+      track.stop();
+    });
+  }
   useEffect(() => {
     if (typeof currentStream.current !== "undefined") {
       stopMediaTracks(currentStream.current);
     }
     startCam();
   }, [startCam]);
+
+  const gotoBack = useCallback(() => {
+    vdRef.current.pause();
+    vdRef.current.src = "";
+    history.push("/");
+  }, [vdRef, history]);
 
   return (
     <div className="appUi scanPage">
@@ -53,6 +121,9 @@ const ScanHand = () => {
             muted
           ></video>
         </div>
+        <button type="button" onClick={gotoBack} className="btn">
+          Go Back
+        </button>
       </div>
     </div>
   );
